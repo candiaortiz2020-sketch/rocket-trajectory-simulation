d99a9a8807b230265884ed9e830748901480baa5
candiaortiz2020-sketch <candia.ortiz2020@gmail.com>
2026-02-03 14:51:31 -0500

    Add air resistance to rocket simulation

--- rocket_simulation.py
+++ rocket_simulation.py
@@ -9,6 +9,12 @@ mass = 50.0        # kg
 thrust = 1200.0    # N
 burn_time = 5.0    # seconds
 
+# Aerodynamic parameters
+rho = 1.225        # air density (kg/m^3)
+Cd = 0.75          # drag coefficient
+radius = 0.15      # rocket radius (m)
+A = np.pi * radius**2
+
 # Simulation parameters
 dt = 0.01
 time = np.arange(0, 15, dt)
@@ -21,15 +27,22 @@ heights = []
 velocities = []
 
 for t in time:
-    if t <= burn_time:
-        force = thrust - mass * g
-    else:
-        force = - mass * g
+    # Drag force
+    drag = 0.5 * rho * Cd * A * velocity**2
+    drag_force = -np.sign(velocity) * drag
 
+    # Net force
+    if t <= burn_time:
+        force = thrust - mass * g + drag_force
+    else:
+        force = - mass * g + drag_force
+
+    # Motion equations
     acceleration = force / mass
     velocity += acceleration * dt
     height += velocity * dt
 
+    # Prevent going below ground
     if height < 0:
         height = 0
         velocity = 0
@@ -42,6 +55,6 @@ plt.figure()
 plt.plot(time, heights)
 plt.xlabel("Time (s)")
 plt.ylabel("Height (m)")
-plt.title("Rocket Vertical Trajectory Simulation")
+plt.title("Rocket Vertical Trajectory with Air Resistance")
 plt.grid()
 plt.show()
