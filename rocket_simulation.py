ae40a49587a6c436f4d6e11896e24a9a68cc3e9b
candiaortiz2020-sketch <candia.ortiz2020@gmail.com>
2026-02-03 15:00:07 -0500

    Add rocket trajectory comparison with and without air resistance

--- rocket_simulation.py
+++ rocket_simulation.py
@@ -19,42 +19,46 @@ A = np.pi * radius**2
 dt = 0.01
 time = np.arange(0, 15, dt)
 
-# Initial conditions
-velocity = 0.0
-height = 0.0
+def simulate(with_drag):
+    velocity = 0.0
+    height = 0.0
+    heights = []
 
-heights = []
-velocities = []
+    for t in time:
+        if with_drag:
+            drag = 0.5 * rho * Cd * A * velocity**2
+            drag_force = -np.sign(velocity) * drag
+        else:
+            drag_force = 0.0
 
-for t in time:
-    # Drag force
-    drag = 0.5 * rho * Cd * A * velocity**2
-    drag_force = -np.sign(velocity) * drag
+        if t <= burn_time:
+            force = thrust - mass * g + drag_force
+        else:
+            force = - mass * g + drag_force
 
-    # Net force
-    if t <= burn_time:
-        force = thrust - mass * g + drag_force
-    else:
-        force = - mass * g + drag_force
+        acceleration = force / mass
+        velocity += acceleration * dt
+        height += velocity * dt
 
-    # Motion equations
-    acceleration = force / mass
-    velocity += acceleration * dt
-    height += velocity * dt
+        if height < 0:
+            height = 0
+            velocity = 0
 
-    # Prevent going below ground
-    if height < 0:
-        height = 0
-        velocity = 0
+        heights.append(height)
 
-    heights.append(height)
-    velocities.append(velocity)
+    return heights
 
-# Plot results
+# Run simulations
+height_no_drag = simulate(with_drag=False)
+height_with_drag = simulate(with_drag=True)
+
+# Plot comparison
 plt.figure()
-plt.plot(time, heights)
+plt.plot(time, height_no_drag, label="Without air resistance")
+plt.plot(time, height_with_drag, label="With air resistance")
 plt.xlabel("Time (s)")
 plt.ylabel("Height (m)")
-plt.title("Rocket Vertical Trajectory with Air Resistance")
+plt.title("Rocket Trajectory Comparison")
+plt.legend()
 plt.grid()
 plt.show()
